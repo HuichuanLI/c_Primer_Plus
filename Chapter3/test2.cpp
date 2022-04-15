f66a87b17f94f93b51047b124f9285d30557d9d1
huichuan <lhc14124908@163.com>
2022-04-15 23:40:26 +0800

    add

--- Chapter3/test2.cpp
+++ Chapter3/test2.cpp
@@ -2,3 +2,18 @@
 // Created by 李汇川 on 2022/4/15.
 //
 
+#include<iostream>
+int main() {
+  using namespace std;
+  const int ArSize = 20;
+  char name[ArSize];
+  char dessert[ArSize];
+
+  cout << "Enter your name\n";
+  cin.getline(name, ArSize);
+  cout << "Enter your favorite dessert\n";
+  cin.getline(dessert, ArSize);
+  cout << "I have some delicious " << dessert;
+  cout << " for you, " << name << ".\n";
+  return 0;
+}
