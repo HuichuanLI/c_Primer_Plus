e86878f164e26fc2b9bc060bebe116228fa03f85
huichuan <lhc14124908@163.com>
2022-04-20 00:53:39 +0800

    add

---
+++ c11_c14_c17_c20/Chapter4/4.2.unordered.map.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 李汇川 on 2022/4/20.
+//
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <map>
+
+int main() {
+  // initialized in same order
+  std::unordered_map<int, std::string> u = {
+      {1, "1"},
+      {3, "3"},
+      {2, "2"}
+  };
+  std::map<int, std::string> v = {
+      {1, "1"},
+      {3, "3"},
+      {2, "2"}
+  };
+
+  // iterates in the same way
+  std::cout << "std::unordered_map" << std::endl;
+  for (const auto &n: u)
+    std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
+
+  std::cout << std::endl;
+  std::cout << "std::map" << std::endl;
+  for (const auto &n: v)
+    std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
+}
