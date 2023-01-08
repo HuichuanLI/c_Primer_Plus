41edd78eca248a0485ab147d354761320c306cc3
lhc456 <lhc14124908@163.com>
2023-01-09 00:53:06 +0800

    add

---
+++ 多线程/test_simple_queue_1.cpp
@@ -0,0 +1,35 @@
+//
+// Created by lhc456 on 2023/1/9.
+//
+#include "SimpleSyncQueue.hpp"
+
+#include <iostream>
+#include <thread>
+
+SimpleSyncQueue<int> syncQueue;
+
+void PutDatas() {
+    for (int i = 0; i < 20; ++i) {
+        syncQueue.Put(888);
+    }
+}
+
+void TakeDatas() {
+    int x = 0;
+
+    for (int i = 0; i < 20; ++i) {
+        syncQueue.Take(x);
+        std::cout << x << std::endl;
+    }
+}
+
+int main(void) {
+    std::thread t1(PutDatas);
+    std::thread t2(TakeDatas);
+
+    t1.join();
+    t2.join();
+
+    system("pause");
+    return 0;
+}
